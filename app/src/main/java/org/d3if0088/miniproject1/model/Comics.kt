4d139c132d381337afe92c3510f6d4be0ebb9139
Muhammad Ihsan Firjatulloh Al Khasaf <muhihsanfa@gmail.com>
2024-05-09 23:59:34 +0700

    Menambahkan model

---
+++ app/src/main/java/org/d3if0088/miniproject1/model/Comics.kt
@@ -0,0 +1,9 @@
+package org.d3if0088.miniproject1.model
+
+data class Comics(
+    val id: Long = 0L,
+    val title: String,
+    val author: String,
+    val released: String,
+    val genre: String
+)
