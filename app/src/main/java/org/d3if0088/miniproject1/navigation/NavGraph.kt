54887db25ed3d3264f076966b3ffa61a6d7277ab
Muhammad Ihsan Firjatulloh Al Khasaf <muhihsanfa@gmail.com>
2024-05-10 15:50:38 +0700

    Menambahkan navigation

--- app/src/main/java/org/d3if0088/miniproject1/navigation/NavGraph.kt
+++ app/src/main/java/org/d3if0088/miniproject1/navigation/NavGraph.kt
@@ -2,27 +2,44 @@ package org.d3if0088.miniproject1.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import org.d3if0088.miniproject1.ui.screen.AboutScreen
+import org.d3if0088.miniproject1.ui.screen.DetailScreen
+import org.d3if0088.miniproject1.ui.screen.KEY_ID_COMICS
 import org.d3if0088.miniproject1.ui.screen.LandingScreen
 import org.d3if0088.miniproject1.ui.screen.MainScreen
+
 
 @Composable
 fun SetupNavGraph(navController: NavHostController = rememberNavController()){
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.Landing.route
     ) {
-        composable(route = Screen.Home.route){
+        composable(route = Screen.Landing.route){
             LandingScreen(navController)
         }
-//        composable(route = Screen.Temperature.route){
-//            MainScreen(navController)
-//        }
+        composable(route = Screen.Home.route){
+            MainScreen(navController)
+        }
         composable(route = Screen.About.route){
             AboutScreen(navController)
+        }
+        composable(route = Screen.FormBaru.route){
+            DetailScreen(navController)
+        }
+        composable(
+            route = Screen.FormUbah.route,
+            arguments = listOf(
+                navArgument(KEY_ID_COMICS) { type = NavType.LongType }
+            )
+        ) {navBackStackEntry ->
+            val id = navBackStackEntry.arguments?.getLong(KEY_ID_COMICS)
+            DetailScreen(navController, id)
         }
     }
 }
--- app/src/main/java/org/d3if0088/miniproject1/navigation/Screen.kt
+++ app/src/main/java/org/d3if0088/miniproject1/navigation/Screen.kt
@@ -1,7 +1,13 @@
 package org.d3if0088.miniproject1.navigation
 
-sealed class Screen (val route:String){
+import org.d3if0088.miniproject1.ui.screen.KEY_ID_COMICS
+
+sealed class Screen (val  route: String){
+    data object Landing: Screen("landingScreen")
     data object Home: Screen("mainScreen")
-//    data object Temperature: Screen("temperatureScreen")
     data object About: Screen("aboutScreen")
+    data object FormBaru: Screen("detailScreen")
+    data object FormUbah: Screen("detailScreen/{$KEY_ID_COMICS}") {
+        fun withId(id: Long) = "detailScreen/$id"
+    }
 }
